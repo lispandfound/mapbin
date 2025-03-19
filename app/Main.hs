a39a177451476ef582945a4dc4471e9091bf5f5d
Jake Faulkner <jakefaulkn@gmail.com>
2025-03-19 18:29:40 +1300

    add stub upload endpoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,23 @@
 module Main where
 
+import Data.Aeson (encode)
+import qualified Data.ByteString.Lazy as B
+import Data.Geospatial
 import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Views.Map
 import Views.Upload
 import Web.Scotty
 
+saveGeoJson :: FilePath -> GeoFeatureCollection Float -> IO ()
+saveGeoJson path = B.writeFile path . encode
+
 main :: IO ()
 main = scotty 3000 $ do
     get "/" $ do
         html . renderHtml $ homePage
-
+    post "/upload" $ do
+        geoJson <- jsonData
+        let path = "maps/test.geojson"
+        liftIO $ saveGeoJson path geoJson
     get "/map/:id" $ do
-        html . renderHtml $ mapViewPage "1" "My Map" "Anonymous" False
+        html . renderHtml $ mapViewPage "test" "My Map" "Anonymous" False
